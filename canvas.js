30b762406a8bd93ce61fe42e6c9d95100915cccd
Adam Liu <cirnochi@gmail.com>
2017-10-28 16:09:43 -0700

    ClickAction implemented

--- canvas.js
+++ canvas.js
@@ -1,44 +1,51 @@
 var canvas = document.querySelector('canvas');
-
-
-canvas.width = window.innerWidth - 2;
-canvas.height = window.innerHeight - 2;
-
-var maxWidth = window.screen.availWidth;
-var maxHeight = window.screen.availHeight;
-
-var mouse = {
+var gridPos = {
   x: undefined,
   y: undefined
 };
 
 
-//Listeners
-window.addEventListener('resize', function() {
-  canvas.width = window.innerWidth - 2;
-  canvas.height = window.innerHeight - 2;
+var clickAction = {
+  state: false,
+  action: false,
+  startx: undefined,
+  starty: undefined,
+  endx: undefined,
+  endy: undefined
+};
 
-});
+canvas.width = window.innerWidth - 2;
+canvas.height = window.innerHeight - 2;
+canvas.onmousemove = function(event) {
+    gridPos.x = Math.floor(event.x / blocksize);
+    gridPos.y = Math.floor(event.y / blocksize);
+};
 
-window.addEventListener('mousemove', 
-  function(event) {
-    mouse.x = event.x;
-    mouse.y = event.y;
-    console.log(mouse);
-});
+canvas.onmousedown = function(event) {
+  clickAction.state = true;
+  clickAction.action = false;
+  clickAction.startx = gridPos.x;
+  clickAction.starty = gridPos.x;
+};
+canvas.onmouseup = function(event) {
+  clickAction.state = false;
+  clickAction.endx = gridPos.x;
+  clickAction.endy = gridPos.x;
+  if ( clickAction.endy - clickAction.starty == 0 && clickAction.endx - clickAction.startx == 0){
+    clickAction.action = true;
+  }
+  console.log(clickAction);
+};
+
 
 
 // Initialize variables
+var maxWidth = window.screen.availWidth;
+var maxHeight = window.screen.availHeight;
 var c = canvas.getContext('2d');
-var cellgap = 2;
 var blocksize = 40;
-var cellsize = blocksize - 2*cellgap;
-
-var colorArray = [
-  '#169185'
-]
-
 var notAlive = '#169185';
+var hover_notAlive = '#9FB8A8'
 var alive = '#F3F5F1';
 
 
@@ -47,22 +54,54 @@ function Cell(x, y) {
   this.x = x;
   this.y = y;
   this.alive = false;
+
+  /* Draw function */
   this.draw = function() {
     //c.fillStyle = colorArray[Math.floor(Math.random() * colorArray.length)]
-    if(this.alive){
-      c.fillStyle = alive;
+
+    //Cell appearance while mouse hovering
+    if(gridPos.x*blocksize == this.x && gridPos.y*blocksize == this.y){
+
+      /*Flip alive status when clicked*/
+      if(clickAction.action){
+        clickAction.action = false;
+        this.alive = !this.alive;
+      }
+      
+      if(clickAction.state){
+        this.cellgap = 4;
+      }
+      else{
+        this.cellgap = 0;
+      }
+      if(this.alive){
+        c.fillStyle = alive;
+      }
+      else{
+        c.fillStyle = notAlive;
+      }
     }
+    // Cell appearance while mouse not hovering
     else{
-      c.fillStyle = notAlive;
-    }    
-    c.fillRect(x+2*cellgap, y+2*cellgap, cellsize, cellsize);
+      this.cellgap = 2;
+      if(this.alive){
+        c.fillStyle = alive;
+      }
+      else{
+        c.fillStyle = notAlive;
+      }
+    }
+
+    //draw the cell
+    this.cellsize = blocksize - 2*this.cellgap;
+    c.fillRect(x+this.cellgap, y+this.cellgap, this.cellsize, this.cellsize);
   }
 }
 
 // Grid
 function Grid(width, height) {
-  this.width = Math.floor(width / cellsize);
-  this.height = Math.floor(height / cellsize);
+  this.width = Math.floor(width / blocksize);
+  this.height = Math.floor(height / blocksize);
   this.grid = new Array(this.width);
 
   for(var x = 0; x < this.width; x++){
@@ -86,12 +125,14 @@ function Grid(width, height) {
     }
   }
 
+  /* Update next Iteration of board */
   this.update = function() {
     var currrent_cell;
+    var cellNeighbors;
     for (var x = 0; x < this.width; x++) {
       for(var y = 0; y < this.height; y++) {
         currrent_cell = this.grid[x][y];
-
+        cellNeighbors = this.getNeighbor(x,y);
       }
     }
   }
@@ -144,22 +185,32 @@ function Grid(width, height) {
 }
 
 
-
-
 var board = new Grid(maxWidth, maxHeight);
 board.setup();
 board.draw();
-console.log(board.getNeighbor(0,1));
+var lastupdate = Date.now();
 
 function animate(){
-
-  setTimeout(
-    function (){
-      requestAnimationFrame(animate)
-      c.clearRect(0,0,innerWidth,innerHeight);
-      board.draw();
-    }, 250);  
+  requestAnimationFrame(animate)
+  c.clearRect(0,0,innerWidth,innerHeight);
+  var currentTime = Date.now()
+  //check if 250 MS passed, update board
+  if ( currentTime - lastupdate > 250){
+    board.update();
+    lastupdate = currentTime;
+  }
+  board.draw();
+  //console.log(board.getNeighbor(1,1));
 }
 
 animate();
+
+//Listeners
+window.addEventListener('resize', function() {
+  canvas.width = window.innerWidth - 2;
+  canvas.height = window.innerHeight - 2;
+  board.draw();
+});
+
+
 
