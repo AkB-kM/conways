ba8b6e4d45e7d1211fe6785b0cc8dff6a2db7633
Adam Liu <cirnochi@gmail.com>
2017-10-28 13:33:12 -0700

    Resizable canvas, animate grid, neighbors

--- canvas.js
+++ canvas.js
@@ -1,17 +1,46 @@
 var canvas = document.querySelector('canvas');
 
+
 canvas.width = window.innerWidth - 2;
 canvas.height = window.innerHeight - 2;
 
+var maxWidth = window.screen.availWidth;
+var maxHeight = window.screen.availHeight;
+
+var mouse = {
+  x: undefined,
+  y: undefined
+};
+
+
+//Listeners
+window.addEventListener('resize', function() {
+  canvas.width = window.innerWidth - 2;
+  canvas.height = window.innerHeight - 2;
+
+});
+
+window.addEventListener('mousemove', 
+  function(event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+    console.log(mouse);
+});
+
+
 // Initialize variables
 var c = canvas.getContext('2d');
-
 var cellgap = 2;
-var cellsize = 50 - 2*cellgap;
+var blocksize = 40;
+var cellsize = blocksize - 2*cellgap;
 
 var colorArray = [
   '#169185'
 ]
+
+var notAlive = '#169185';
+var alive = '#F3F5F1';
+
 
 // Cell object
 function Cell(x, y) {
@@ -19,7 +48,13 @@ function Cell(x, y) {
   this.y = y;
   this.alive = false;
   this.draw = function() {
-    c.fillStyle = colorArray[Math.floor(Math.random() * colorArray.length)]
+    //c.fillStyle = colorArray[Math.floor(Math.random() * colorArray.length)]
+    if(this.alive){
+      c.fillStyle = alive;
+    }
+    else{
+      c.fillStyle = notAlive;
+    }    
     c.fillRect(x+2*cellgap, y+2*cellgap, cellsize, cellsize);
   }
 }
@@ -28,44 +63,103 @@ function Cell(x, y) {
 function Grid(width, height) {
   this.width = Math.floor(width / cellsize);
   this.height = Math.floor(height / cellsize);
-  this.grid = new Array(this.width * this.height)
-  this.draw = function() {
+  this.grid = new Array(this.width);
 
-    //Draw grid
+  for(var x = 0; x < this.width; x++){
+    this.grid[x] = new Array(this.height);
+  }
+
+  /* Setup board */
+  this.setup = function() { 
     for (var x = 0; x < this.width; x++) {
       for(var y = 0; y < this.height; y++) {
-
-        //WOWEFASDFASDF
-        //SDFASDF
-        this.grid[y * this.width + x] = new Cell(x*50, y*50);
-        this.grid[y * this.width + x].draw();
-
-        //c.shadowColor = '#999';
-        //c.shadowBlur = 20;
-        //c.shadowOffsetX = 3;
-        //c.shadowOffsetY = 3;
+        this.grid[x][y] = new Cell(x*blocksize, y*blocksize);
       }
     }
   }
+  /* Draw the board */
+  this.draw = function() {
+    for (var x = 0; x < this.width; x++) {
+      for(var y = 0; y < this.height; y++) {
+        this.grid[x][y].draw();
+      }
+    }
+  }
+
+  this.update = function() {
+    var currrent_cell;
+    for (var x = 0; x < this.width; x++) {
+      for(var y = 0; y < this.height; y++) {
+        currrent_cell = this.grid[x][y];
+
+      }
+    }
+  }
+
+  /* Get Count of Neighbors alive */
+  this.getNeighbor = function (x,y){
+    var count = 0;
+    if ( x - 1 >= 0){ // LEFT
+      if(this.grid[x - 1][y].alive){
+        count++;
+      }
+    }
+    if ( x - 1 >= 0 && y - 1 >= 0){ // UPPER LEFT
+      if(this.grid[x - 1][y - 1].alive){
+        count++;
+      }
+    }
+    if ( y - 1 >= 0){ // TOP
+      if(this.grid[x][y - 1].alive){
+        count++;
+      }
+    }
+    if ( x + 1 < this.width && y - 1 >= 0){ // UPPER RIGHT
+      if(this.grid[x + 1][y - 1].alive){
+        count++;
+      }
+    }
+    if ( x + 1 < this.width){ // RIGHT
+      if(this.grid[x + 1][y].alive){
+        count++;
+      }
+    }
+    if ( x + 1 < this.width && y + 1 < this.height){ // LOWER RIGHT
+      if(this.grid[x + 1][y + 1].alive){
+        count++;
+      }
+    }
+    if ( y + 1 < this.height){ //BOTTOM
+      if(this.grid[x][y + 1].alive){
+        count++;
+      }
+    }
+    if ( x - 1 >= 0 && y + 1 < this.height){ // LOWER LEFT
+      if(this.grid[x - 1][y + 1].alive){
+        count++;
+      }
+    }
+    return count;    
+  }
 }
 
-var board = new Grid(canvas.width, canvas.height);
+
+
+
+var board = new Grid(maxWidth, maxHeight);
+board.setup();
 board.draw();
+console.log(board.getNeighbor(0,1));
 
-//
-// Window Resizing Event Listener
-window.addEventListener('resize', function() {
-  canvas.width = window.innerWidth - 2;
-  canvas.height = window.innerHeight - 2;
-  var gridx = Math.floor(canvas.width / cellsize);
-  var gridy = Math.floor(canvas.height / cellsize);
-});
+function animate(){
 
-var x = 200;
-function animate() {
-  requestAnimationFrame(animate);
-
-
+  setTimeout(
+    function (){
+      requestAnimationFrame(animate)
+      c.clearRect(0,0,innerWidth,innerHeight);
+      board.draw();
+    }, 250);  
 }
 
 animate();
+
